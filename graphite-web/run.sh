cb2884c60e1f43a1e7442c81050f54c515a11cf2
Johannes Drummer <j.drummer@thelambda.de>
2019-09-26 17:16:37 +0200

    use build container for smaller production images

--- graphite-web/run.sh
+++ graphite-web/run.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-/usr/bin/django-admin migrate --settings=graphite.settings --run-syncdb
+
+/opt/graphite/bin/django-admin migrate --settings=graphite.settings --run-syncdb
+#/usr/bin/django-admin migrate --settings=graphite.settings --run-syncdb
 
 cd /opt/graphite/conf
 exec /usr/bin/gunicorn -w 4 -b 0.0.0.0:8080 wsgi:application 
