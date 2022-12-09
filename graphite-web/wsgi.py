769f5d523203a238073bbe65a5c90f5d5f58f53d
nook24 <info@nook24.eu>
2022-12-09 09:26:50 +0000

    Update Graphite Web to 1.1.10 and set Alpine Linux version to 1.17

--- graphite-web/wsgi.py
+++ graphite-web/wsgi.py
@@ -1,7 +1,12 @@
 import sys
-from whitenoise.django import DjangoWhiteNoise
+
+# Packport of https://github.com/graphite-project/graphite-web/pull/2333
+# Configure of WhiteNoise 4.1.4 is done by middleware in app_settings.py
+# http://whitenoise.evans.io/en/stable/changelog.html#v4-0
+# See for old WhiteNoise 3.x version: https://github.com/it-novum/graphing-docker/blob/10f22f6c251d2b6ef2cc33d5c5e2a41d1ae5d3f7/graphite-web/wsgi.py
+
 
 sys.path.append('/opt/graphite/webapp')
-from graphite.wsgi import application as graphapp
-application = DjangoWhiteNoise(graphapp)
 
+from graphite.wsgi import application as graphapp
+application = graphapp
